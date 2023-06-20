baf595b0d38057ef15e2103bdbe7094a399a47e0
painiago <95057970+painiago@users.noreply.github.com>
2023-06-19 23:19:33 -0300

    Adicinando Scroll to top button

---
+++ src/components/ScrollBtn/ScrollToTopButton.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+import './ScrollToTopButton.scss';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    setIsVisible(scrollTop > 400);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <button
+      className={`scroll-to-top-button ${isVisible ? "visible" : ""}`}
+      onClick={scrollToTop}
+    >
+      <FontAwesomeIcon icon={faChevronUp} />
+    </button>
+  );
+};
+
+export default ScrollToTopButton;
